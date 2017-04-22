6fcacbf389c7c66a056ab87946a1d2ad92ced7ca
Polyglot <winfixit@gmail.com>
2017-04-22 12:38:09 +0200

    I'm adding a model for stops, it's far from finished, more brainstorming than anything else

--- stops/models.py
+++ stops/models.py
@@ -1,3 +1,101 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.contrib.gis.db import models
+
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    route_ref = models.TextField(blank=True, null=True)
+    zone = models.IntegerField(blank=True, null=True)
+    geomdl = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+class Stop(models.Model):
+    '''This model tracks changes. Data from OSM and the various operators comes in through discardable tables. That data is then converted and stored here. If data for this stop was already present in this table, the field gets updated. If this means that something should be changed in OSM, the changed_by_operator flag gets set. I'm not sure at the moment whether it should be more specific: name changed, location changed, abolished
+Changes can happen in OSM as well.
+It's then up to the user to decide how to act upon these changes. If the user makes changes, they should get a link to go and apply them in JOSM.
+
+Use cases:
+stop exists in OSM
+change comes in through one of the operators
+fields are compared and updated if needed, changed_by_operator flag gets set
+user gets change to make changes using JOSM_RC, with updated data
+
+stop exists in OSM
+some tags are modified or it is moved
+fields are compared, change is detected and changed_in_osm flag gets set
+user gets change to make changes using JOSM_RC, with updated data
+
+stop is removed from OSM
+we detect this based on OSM id, now we need to look if the operator still has it
+give user possibility to revert deletion or to remove our record
+
+stop is removed from operator's DB
+check if this was the only operator for that stop, give user possibility to delete it using JOSM
+JOSM RC probably should only do zoom_and_download
+if the stop is served by more than 1 operator, change the tags accordingly.
+
+stop is added in operator's DB
+no stop with corresponding ref is found in OSM, this should be a separate view to list these
+
+There should also be a view where one can see that the distance between OSM and where the operator thinks their stop is,is more than 5 meters
+
+There should be a possibility to mark certain differences as OK
+* sometimes the names coming from the operator aren't what we want to use in OSM
+* sometimes the positions are off
+
+ref and route_ref should always be updated in OSM, operator should be authoritative on those
+
+'''
+
+
+
+    changed_by_user = models.BooleanField()
+    changed_in_osm = models.BooleanField()
+    changed_by_operator = models.BooleanField()
+    osm_id = models.BigIntegerField(primary_key=True)
+    osm_type = models.TextField(blank=True, null=True)
+    # Geo Django field to store a point
+    osm_geom = models.PointField(help_text="Represented as (longitude, latitude)")
+    timestamp = models.DateTimeField(blank=True, null=True)
+    version = models.IntegerField(blank=True, null=True)
+    username = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    name_de_lijn = models.TextField(blank=True, null=True)
+    name_tec = models.TextField(blank=True, null=True)
+    name_nl = models.TextField(blank=True, null=True)
+    name_fr = models.TextField(blank=True, null=True)
+    name_en = models.TextField(blank=True, null=True)
+    name_de = models.TextField(blank=True, null=True)
+    ref = models.TextField(blank=True, null=True)
+    ref_de_lijn = models.TextField(blank=True, null=True)
+    ref_tecb = models.TextField(blank=True, null=True)
+    ref_tecc = models.TextField(blank=True, null=True)
+    ref_tech = models.TextField(blank=True, null=True)
+    ref_tecn = models.TextField(blank=True, null=True)
+    ref_tecl = models.TextField(blank=True, null=True)
+    ref_tecx = models.TextField(blank=True, null=True)
+    route_ref = models.TextField(blank=True, null=True)
+    route_ref_de_lijn = models.TextField(blank=True, null=True)
+    route_ref_tecb = models.TextField(blank=True, null=True)
+    route_ref_tecc = models.TextField(blank=True, null=True)
+    route_ref_tech = models.TextField(blank=True, null=True)
+    route_ref_tecn = models.TextField(blank=True, null=True)
+    route_ref_tecl = models.TextField(blank=True, null=True)
+    route_ref_tecx = models.TextField(blank=True, null=True)
+    zone = models.TextField(blank=True, null=True)
+    zone_de_lijn = models.TextField(blank=True, null=True)
+    zone_tec = models.TextField(blank=True, null=True)
+    source = models.TextField(blank=True, null=True)
+    operator = models.TextField(blank=True, null=True)
+    dl_geom = models.PointField(help_text="Represented as (longitude, latitude)")
+    dl_offset = models.FloatField(blank=True, null=True)
+    dl_ref_de_lijn = models.TextField(blank=True, null=True)
+    dl_name = models.TextField(blank=True, null=True)
+    dl_street = models.TextField(blank=True, null=True)
+    #stopisaccessible = models.NullBooleanField()
+
+    # You MUST use GeoManager to make Geo Queries
+    objects = models.GeoManager()
+
+    def __str__(self):
+        return '{} {} {} {}'.format(self.mode, self.publicref, self.operator, self.ref)
+
