687d9708674205513926caa4ed9f49621ab813ab
PolyglotOpenstreetmap <winfixit@gmail.com>
2018-04-26 21:37:36 +0200

    major refactoring as suggested by PyCharm

--- OSM_data_model.py
+++ OSM_data_model.py
@@ -1,5 +1,6 @@
 #!/bin/python
-import OSM_lib as OL
+import OSM_lib as osmlib
+
 
 class MapLayer():
     def __init__(self):
@@ -7,14 +8,14 @@ class MapLayer():
         self.ways = {}
         self.relations = {}
         self.edges = {}
-        self.changed = [] # list of all 'dirty' objects that need to be flagged for upload
+        self.changed = []  # list of all 'dirty' objects that need to be flagged for upload
 
-    def asXML(self):
-        params = {upload: " upload='false'",
-                  generator: " generator='Python script'",
+    def as_xml(self):
+        params = {"upload": " upload='false'",
+                  "generator": " generator='Python script'",
                   }
         xml = '''<?xml version='1.0' encoding='UTF-8'?>\n<osm version='0.6'{upload}{generator}>\n'''.format(**params)
-        print (xml)
+        print(xml)
         for n in self.nodes:
             xml += self.nodes[n].asXML()
         for w in self.ways:
@@ -26,10 +27,12 @@ class MapLayer():
         return xml
 
 
-class OSM_Primitive():
+class Primitive:
+    """Base class with common functionality between Nodes, Ways and Relations"""
     counter = -10000
 
     def __init__(self, ml, primitive, attributes=None, tags=None):
+        self.maplayer = ml
         if attributes:
             self.attributes = attributes
         else:
@@ -40,14 +43,15 @@ class OSM_Primitive():
             self.tags = {}
         self.primitive = primitive
 
+        self.xml = ''
         self.dirty = False
 
-        if not (self.attributes) or not ('id' in self.attributes):
-            print(OSM_Primitive.counter)
+        if not self.attributes or not ('id' in self.attributes):
+            print(Primitive.counter)
             self.attributes['action'] = 'modify'
             self.attributes['visible'] = 'true'
-            self.attributes['id'] = str(OSM_Primitive.counter)
-            OSM_Primitive.counter -= 1
+            self.attributes['id'] = str(Primitive.counter)
+            Primitive.counter -= 1
 
     def __repr__(self):
         r = '\n' + self.primitive + '\n'
@@ -57,64 +61,67 @@ class OSM_Primitive():
             r += "{}: {}\n".format(key, self.tags[key])
         return r
 
-    def addTags(self, tags):
+    def add_tags(self, tags):
         if tags:
             for key in tags:
-                self.addTag(key, tags[key])
+                self.add_tag(key, tags[key])
 
-    def addTag(self, key, value):
+    def add_tag(self, key, value):
         self.tags[key] = value
 
-    def asXML(self, body=''):
+    def as_xml(self, body=''):
         self.xml = '\n<{} '.format(self.primitive)
         for attr in ['id', 'lat', 'lon', 'action', 'timestamp', 'uid', 'user', 'visible', 'version', 'changeset']:
             if attr in self.attributes:
                 if attr == 'timestamp':
                     self.attributes[attr] = str(self.attributes[attr]).replace(' ', 'T') + 'Z'
                 if attr == 'user':
-                    self.attributes[attr] = OL.xmlsafe(self.attributes[attr])
+                    self.attributes[attr] = osmlib.xmlsafe(self.attributes[attr])
                 self.xml += "{}='{}' ".format(attr, str(self.attributes[attr]))
         self.xml += '>'
-        if body: self.xml += body
+        if body:
+            self.xml += body
         for key in self.tags:
-            self.xml += "\n  <tag k='{key}' v='{tag}' />".format(key=key, tag=OL.xmlsafe(self.tags[key]))
+            self.xml += "\n  <tag k='{key}' v='{tag}' />".format(key=key, tag=osmlib.xmlsafe(self.tags[key]))
         self.xml += '\n</{}>'.format(self.primitive)
         return self.xml
 
-    def getParents(self, ml):
+    def get_parents(self):
         parents = []
-        for way in ml.ways:
-            if self['id'] in way.getNodes():
+        for way in self.maplayer.ways:
+            if self.attributes['id'] in way.get_nodes():
                 parents.append(way)
-        for relation in ml.relations:
-            if self['id'] in relation.getMembers():
+        for relation in self.maplayer.relations:
+            if self.attributes['id'] in relation.get_members():
                 parents.append(relation)
         return parents
 
 
-class Node(OSM_Primitive):
+class Node(Primitive):
     def __init__(self, ml, attributes=None, tags=None):
-        if not (attributes):
+        if not attributes:
             attributes = {'lon': '0.0', 'lat': '0.0'}
 
         super().__init__(ml, primitive='node', attributes=attributes, tags=tags)
         ml.nodes[self.attributes['id']] = self
 
 
-class Way(OSM_Primitive):
+class Way(Primitive):
     def __init__(self, ml, attributes=None, nodes=None, tags=None):
-        '''Ways are built up as an ordered sequence of nodes
+        """Ways are built up as an ordered sequence of nodes
            it can happen we only know the id of the node,
-           or we might have a Node object with all the details'''
+           or we might have a Node object with all the details"""
         super().__init__(ml, primitive='way', attributes=attributes, tags=tags)
+
         self.nodes = []
-        if nodes: self.addNodes(nodes)
+        if nodes:
+            self.add_nodes(nodes)
         ml.ways[self.attributes['id']] = self
         self.closed = None
-        self.incomplete = None # not all nodes are downloaded
+        self.incomplete = None  # not all nodes are downloaded
 
     def __repr__(self):
-        body = super().asXML()
+        body = super().__repr__()
         for node in self.nodes:
             body += "\n  {node_id}".format(node_id=node)
         return body
@@ -125,13 +132,13 @@ class Way(OSM_Primitive):
     def __getitem__(self, position):
         return self.nodes[position]
 
-    def addNodes(self, nodes):
+    def add_nodes(self, nodes):
         if nodes:
             for n in nodes:
-                self.addNode(n)
-        self.isClosed()
+                self.add_node(n)
+        self.is_closed()
 
-    def addNode(self, node):
+    def add_node(self, node):
         try:
             ''' did we receive an object instance to work with? '''
             n = node.attributes['id']
@@ -140,23 +147,22 @@ class Way(OSM_Primitive):
             n = node
         self.nodes.append(str(n))
 
-    def asXML(self):
-        body = ''
+    def as_xml(self, body=''):
         for node in self.nodes:
             body += "\n  <nd ref='{node_id}' />".format(node_id=node)
-        return super().asXML(body=body)
+        return super().as_xml(body=body)
 
-    def isClosed(self):
-        self.clsoed = False
+    def is_closed(self):
+        self.closed = False
         if self.nodes[0] == self.nodes[-1]:
             self.closed = True
 
 
-class RelationMember():
+class RelationMember:
     def __init__(self, role='', primtype='', member=None):
         self.primtype = primtype
-        if role == None:
-            self.role=''
+        if role is None:
+            self.role = ''
         else:
             self.role = role
         try:
@@ -166,17 +172,17 @@ class RelationMember():
                 ''' did we receive an object instance to work with? '''
                 m = member.attributes['id']
                 self.primtype = member.primitive
-            except (KeyError, NameError) as e:
+            except (KeyError, NameError):
                 ''' the member id was passed as a string or an integer '''
                 m = member
         self.memberid = str(m)
 
-    def asXML(self):
+    def as_xml(self):
         return "\n  <member type='{primtype}' ref='{ref}' role='{role}' />".format(
             primtype=self.primtype, ref=self.memberid, role=self.role)
 
 
-class Relation(OSM_Primitive):
+class Relation(Primitive):
     def __init__(self, ml, members=None, tags=None, attributes=None):
         super().__init__(ml, primitive='relation', attributes=attributes, tags=tags)
 
@@ -186,113 +192,122 @@ class Relation(OSM_Primitive):
             self.members = members
 
         ml.relations[self.attributes['id']] = self
-        self.incomplete = None # not all members are downloaded
+        self.incomplete = None  # not all members are downloaded
 
     def __repr__(self):
         r = super().__repr__()
         return r
 
-    def addMembers(self, members):
+    def add_members(self, members):
         if members:
             for m in members:
-                self.addMember(m)
+                self.add_member(m)
 
-    def addMember(self, member):
+    def add_member(self, member):
         self.members.append(member)
 
-    def asXML(self):
-        body = ''
+    def as_xml(self, body=''):
         for member in self.members:
             body += member.asXML()
 
-        return super().asXML(body=body)
+        return super().as_xml(body=body)
 
 
-class PT_Stop(Node):
-    '''In this model a public transport stop is always mapped on a node with public_transport=platform tag
+class PublicTransportStop(Node):
+    """"In this model a public transport stop is always mapped on a node with public_transport=platform tag
+
        This is a simplification, which makes sure there are always coordinates. In most cases this node
-       represents the pole to which the flag with all details for the stop is mounted'''
+       represents the pole to which the flag with all details for the stop is mounted"""
 
     def __init__(self, ml, attributes=None, tags=None):
         super().__init__(ml, attributes, tags)
-        if tags == None:
+        if tags is None:
             self.tags['highway'] = 'bus_stop'
             self.tags['public_transport'] = 'platform'
 
 
-class PT_StopArea(Relation):
+class PublicTransportStopArea(Relation):
     pass
 
 
-class PT_Route(Relation):
-    '''This is what we think of as a variation of a line'''
+class PublicTransportRoute(Relation):
+    """This is what we think of as a variation of a line"""
 
     def __init__(self, ml, members=None, tags=None, attributes=None):
+        self.members = members
         tags['type'] = 'route'
         print('attr PT route: ', attributes)
         super().__init__(ml, attributes=attributes, tags=tags)
         self.continuous = None
 
-    def isContinuous(self):
-        lastNodeOfPreviousWay = None
+    def is_continuous(self):
+        last_node_of_previous_way = None
         for member in self.members:
             if member.primitive == 'way':
                 ''' Is this way present in the downloaded data?'''
-                if  not(member.memberid in ml.ways):
+                if not (member.memberid in self.maplayer.ways):
                     self.continuous = None
                     return None
                 ''' First time in loop, just store last node of way as previous node'''
-                if lastNodeOfPreviousWay == None:
-                    lastNodeOfPreviousWay = ml.ways[member.memberid][-1]
+                if last_node_of_previous_way == None:
+                    last_node_of_previous_way = self.maplayer.ways[member.memberid][-1]
                     continue
                 else:
-                    if lastNodeOfPreviousWay != ml.ways[member.memberid][0]:
+                    if last_node_of_previous_way != self.maplayer.ways[member.memberid][0]:
                         self.continuous = False
                         return False
         '''If we get here, the route is continuous'''
         self.continuous = True
         return True
 
-class PT_RouteMaster(Relation):
-    '''This is what we think of as a public transport line
-       It contains route relations for each variation of an itinerary'''
+
+class PublicTransportRouteMaster(Relation):
+    """This is what we think of as a public transport line
+       It contains route relations for each variation of an itinerary"""
 
     def __init__(self, ml, members=None, tags=None, attributes=None):
-        tags['type'] = 'route_master'
-        super().__init__(ml, attributes=attributes, tags=tags)
+        if members is None:
+            self.members = []
+        else:
+            self.members = members
+        if tags is None:
+            self.tags = []
+        else:
+            self.tags = tags
+        self.tags['type'] = 'route_master'
+        super().__init__(ml, attributes=attributes, tags=self.tags)
 
-    def addRoute(self, route):
-        m = RelationMember(primtype='relation', role='', member=route)
-        super().addMember(m)
+    def add_route(self, route):
+        super().add_member(RelationMember(primtype='relation', role='', member=route))
 
 
-class Edge():
-    '''An edge is a sequence of ways that are connected to one another, they can either be between where highways fork,
-       or where PT routes fork. An edge can contain shorter edges'''
+class Edge:
+    """An edge is a sequence of ways that are connected to one another, they can either be between where highways fork,
+       or where PT routes fork. An edge can contain shorter edges"""
 
     def __init__(self, ml, parts=None):
         self.parts = []
         if parts:
-            self.parts = self.addParts(parts)
+            self.parts = self.add_parts(parts)
         # ml.edges[self.attributes['id']] = self
 
-    def addParts(self, parts):
+    def add_parts(self, parts):
         for p in parts:
-            self.addPart(p)
+            self.add_part(p)
 
-    def addPart(self, part):
+    def add_part(self, part):
         self.parts.append(part)
 
-    def getWays(self):
+    def get_ways(self):
         ways = []
         if self.parts:
             for p in self.parts:
                 try:
-                    p.getNodes()
+                    p.get_nodes()
                     ways.append(p)
                 except:
                     try:
-                        ways.extend(p.getWays())
+                        ways.extend(p.get_ways())
                     except:
                         pass
         return ways
