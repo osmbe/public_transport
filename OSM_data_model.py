7ca48254367351b8ee6f5bbdad37250abe743979
PolyglotOpenstreetmap <winfixit@gmail.com>
2018-05-02 10:57:53 +0200

    remove print

--- OSM_data_model.py
+++ OSM_data_model.py
@@ -1,7 +1,9 @@
 #!/bin/python
 import OSM_lib as osmlib
 from urllib.parse import urlencode
-
+from pyproj import Proj, transform
+bel_proj = Proj(init='epsg:31370')
+osm_proj = Proj(init='epsg:4326')
 
 class MapLayer():
     def __init__(self):
@@ -103,7 +105,7 @@ class Primitive:
         self.xml = ''
         self.dirty = False
 
-        if not self.attributes or not ('id' in self.attributes):
+        if not ('id' in self.attributes):
             self.attributes['action'] = 'modify'
             self.attributes['visible'] = 'true'
             self.attributes['id'] = str(Primitive.counter)
