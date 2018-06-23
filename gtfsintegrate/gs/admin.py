d58fb76a44b60cbfa39ca1e26f1ef0326e02cd2b
PolyglotOpenstreetmap <winfixit@gmail.com>
2018-06-23 16:40:23 +0000

    removing gtfsintegrate from this repoository

--- gtfsintegrate/gs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Incidences
-
-class IncidencesAdmin(admin.ModelAdmin):
-	list_display = ('name','location')
-
-admin.site.register(Incidences,IncidencesAdmin)
