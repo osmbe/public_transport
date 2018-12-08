f0be1ae5916b3dddc636df56b697886057d92dfc
PolyglotOpenstreetmap <winfixit@gmail.com>
2018-12-08 11:24:48 +0100

    no maplayer for RelationMember objects

--- scripts/OSM_data_model.py
+++ scripts/OSM_data_model.py
@@ -422,21 +422,18 @@ class Stop:
         elif isinstance(primitive, Node):
             if (pt_tag and primitive.tags['public_transport'] == 'platform' or
                     hw_tag and primitive.tags['highway'] == 'bus_stop'):
-                self.platform_node = RelationMember(self.map_layer,
-                                                    role='platform',
+                self.platform_node = RelationMember(role='platform',
                                                     primitive_type='node',
                                                     member=primitive)
             elif (pt_tag and primitive.tags['public_transport'] == 'stop_position'):
-                self.stop_position_node = RelationMember(self.map_layer,
-                                                         role='stop',
+                self.stop_position_node = RelationMember(role='stop',
                                                          primitive_type='node',
                                                          member=primitive)
         elif isinstance(primitive, Way):
             if (pt_tag and primitive.tags['public_transport'] == 'platform' or
                     hw_tag and primitive.tags['highway'] == 'platform' or
                     rw_tag and primitive.tags['railway'] == 'platform'):
-                self.platform_way = RelationMember(self.map_layer,
-                                                   role='platform',
+                self.platform_way = RelationMember(role='platform',
                                                    primitive_type='way',
                                                    member=primitive)
 
